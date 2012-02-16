e7a4fdd448b529d3205ced090505a93f7551e004
Jonas Klauber <skogs4@hotmail.com>
2012-02-16 15:00:09 +0100

    added setprop

--- src/client/test.java
+++ src/client/test.java
@@ -6,6 +6,10 @@ public class test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		System.setProperty("javax.net.ssl.keyStore", "keystore");
+		System.setProperty("javax.net.ssl.keyStorePassword", "password");
+		System.setProperty("javax.net.ssl.trustStore", "truststore");
+		System.setProperty("javax.net.ssl.truststorPassword", "password");
 		SSLClient c = new SSLClient();
 
 	}
