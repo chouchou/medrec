1317053a01c251c79afae6f50827bafa2193a0c1
orginaldrowzy <simon.thornqvist@gmail.com>
2012-02-16 14:32:33 +0100

    Added some lines for testing

--- src/server/SSLServer.java
+++ src/server/SSLServer.java
@@ -2,7 +2,9 @@ package server;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.security.cert.X509Certificate;
 
 import javax.net.ServerSocketFactory;
@@ -23,22 +25,30 @@ public class SSLServer {
 		
 	}
 	
-	public void listen(int port) throws IOException{
+	private void listen(int port) throws IOException{
 		ServerSocketFactory factory = SSLServerSocketFactory.getDefault();
 		 
 		
 		SSLServerSocket	ss = (SSLServerSocket) factory.createServerSocket(port);
+		ss.setNeedClientAuth(true);
 		SSLSocket connection = (SSLSocket)ss.accept();
+		System.out.println("connection established");
+		establishAndValidate(connection);
+		
+		
+		
+	}
+	
+	private void establishAndValidate(SSLSocket connection) throws IOException{
 		BufferedInputStream input = new BufferedInputStream(connection.getInputStream());
 		BufferedOutputStream output = new BufferedOutputStream(connection.getOutputStream());
-		connection.setNeedClientAuth(true);
 		SSLSession session = connection.getSession();
 		javax.security.cert.X509Certificate cert = session.getPeerCertificateChain()[0];
 		cert.getSerialNumber();
-		
-		
-		
-		
+		BufferedWriter w = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
+		String s = "Hej Hej Hej";
+		w.write(s,0,s.length());
+		w.flush();
 		
 		
 	}
