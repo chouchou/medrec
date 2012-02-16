4b71c6b4803da0c1680f8d8f0d403c1d740f5719
orginaldrowzy <simon.thornqvist@gmail.com>
2012-02-16 13:44:05 +0100

    Added implementation for listening for incoming connections on serverside

--- src/server/SSLServer.java
+++ src/server/SSLServer.java
@@ -1,8 +1,36 @@
 package server;
 
+import java.io.IOException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ServerSocketFactory;
+import javax.net.SocketFactory;
+import javax.net.ssl.*;
+
 public class SSLServer {
 	
-	public SSLserver(){
+	
+	
+	public SSLServer(int port){
+		try {
+			listen(port);
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void listen(int port) throws IOException{
+		ServerSocketFactory factory = SSLServerSocketFactory.getDefault();
+		 
+		
+		SSLServerSocket	ss = (SSLServerSocket) factory.createServerSocket(port);
+		SSLSocket connection = (SSLSocket)ss.accept();
+		SSLSession session = connection.getSession();
+		javax.security.cert.X509Certificate cert = session.getPeerCertificateChain()[0];
+		cert.getSerialNumber();
+		
 		
 	}
 
