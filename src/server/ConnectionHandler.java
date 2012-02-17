d6c69994305d0bd869be6ba3e9a1457b25cb081a
orginaldrowzy <simon.thornqvist@gmail.com>
2012-02-17 16:11:25 +0100

    somechanges

--- src/server/ConnectionHandler.java
+++ src/server/ConnectionHandler.java
@@ -24,45 +24,53 @@ public class ConnectionHandler {
 
 	}
 
-	private void enableCommunication(BufferedWriter writer, BufferedReader reader, String id) throws IOException{
+	private void enableCommunication(BufferedWriter writer,
+			BufferedReader reader, String id) throws IOException {
 		try {
-			writer.write("Communication is now open Use commands: A - " +
-					"For adding new entry V personNumber - " +
-					"For viewing entry with pnbr R personNumber - " +
-					"For removing entry with pnbr\n" + "W - For writing to entyr\n" );
+			writer.write("Communication is now open Use commands: A - "
+					+ "For adding new entry V personNumber - "
+					+ "For viewing entry with pnbr R personNumber - "
+					+ "For removing entry with pnbr\n"
+					+ "W - For writing to entyr\n");
 			writer.flush();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		char command = reader.readLine().toCharArray()[0];
-		 
-		while(command != 0){
-			
-			switch(command){
-			case ADD : 
+
+		while (command != 0) {
+
+			switch (command) {
+			case ADD:
 				writer.write("Specify");
-				writer.write("Adding journal to patient\n");	
-			break;
-			case WRITE : writer.write("Specify filename\n");
+				writer.write("Adding journal to patient\n");
+				break;
+			case WRITE:
+				writer.write("Specify filename\n");
 				writer.flush();
-			String fileName = reader.readLine();
-			writer.write("Write message\n");
-			writer.flush();
-			String message = reader.readLine();
-			String whatHappend = getCurrentUser().writeRecord(Integer.parseInt(id), fileName, message);
-			writer.write(whatHappend);
-			writer.flush();
-			case VIEW : break;
-			case REMOVE : break;
-			case EXIT : startCommunication(writer, reader);
-			default : writer.write("Wrong Format, try again\n");
-			break;
-				
+				String fileName = reader.readLine();
+				writer.write("Write message\n");
+				writer.flush();
+				String message = reader.readLine();
+				String whatHappend = getCurrentUser().writeRecord(
+						Integer.parseInt(id), fileName, message);
+				writer.write(whatHappend);
+				writer.flush();
+			case VIEW:
+				break;
+			case REMOVE:
+				break;
+			case EXIT:
+				startCommunication(writer, reader);
+			default:
+				writer.write("Wrong Format, try again\n");
+				break;
+
 			}
 			writer.flush();
 			command = reader.readLine().toCharArray()[0];
 		}
-		
+
 	}
 
 	private Human getCurrentUser() {
@@ -79,7 +87,7 @@ public class ConnectionHandler {
 			// Close system
 		}
 
-		else if (true) {
+		else if (getCurrentUser().checkAccess(Integer.parseInt(response))) {
 			writer.write("Access granted for patient: " + response);
 			writer.flush();
 			enableCommunication(writer, reader, response);
